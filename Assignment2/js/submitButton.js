183b5c7b4a27105f28ec4a567eb9e96b1c55cafd
febryyadiz <febryyadi.z@yahoo.com>
2022-02-04 03:03:12 +0800

    add assignment 2

---
+++ Assignment2/js/submitButton.js
@@ -0,0 +1,9 @@
+function onSubmit() {
+    document.getElementById('text-name').innerHTML = document.getElementById("input-name").value;
+    document.getElementById('text-role').innerHTML = document.getElementById("input-role").value;
+    document.getElementById('text-availability').innerHTML = document.getElementById("input-availability").value;
+    document.getElementById('text-age').innerHTML = document.getElementById("input-age").value;
+    document.getElementById('text-address').innerHTML = document.getElementById("input-address").value;
+    document.getElementById('text-experiences').innerHTML = document.getElementById("input-experiences").value;
+    document.getElementById('text-email').innerHTML = document.getElementById("input-email").value;
+};
